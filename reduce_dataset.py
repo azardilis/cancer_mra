83e329dbda31345f9ec6ae982e91a61f901ad5ae
Argyris Zardilis <az325@cam.ac.uk>
2014-02-10 19:36:29 +0000

    added the tfs in order

--- reduce_dataset.py
+++ reduce_dataset.py
@@ -1,19 +1,29 @@
+import sys
 
+def read_annotation(annot_file):
+    ids = dict()
+    for record in open(annot_file):
+        pid, tf = record.split(" ")
+        ids[pid] = tf.strip()
+
+    return ids
 
 def main():
-    ids = set((pid.strip() for pid in open("annotation.dat")))
-    out_file = open("disc_set/discovery_ExpressionMatrix_red.txt", "wb")
-    
-    with open("disc_set/discovery_ExpressionMatrix.txt", "r") as f:
+    ids = read_annotation("annotation.dat")
+
+    out_file = open("data/disc_set/discovery_ExpressionMatrix_red.txt", "wb")
+    tf_file = open("data/disc_set/tfs.txt", "wb")
+    with open("data/disc_set/discovery_ExpressionMatrix.txt", "r") as f:
         header = f.readline()
-        out_file.write(header)
+        out_file.write(header.strip())
         for record in f:
             rec_id = record.split()[0]
             if rec_id in ids:
                 out_file.write(record)
+                tf_file.write(ids[rec_id]+"\n")
 
 
     out_file.close()
-
+    tf_file.close()
 
 main()
