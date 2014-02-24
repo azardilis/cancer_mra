682f26a9b765182a1d96389c757a2ea93c5b2bfb
Argyris Zardilis <azardilis@gmail.com>
2014-02-24 23:40:33 +0000

    deleted the old methods.

--- build_net.R
+++ build_net.R
@@ -1,8 +1,8 @@
-library(psych)
 library(igraph)
 library(corpcor)
+library(GeneNet)
 
-pcorTest <- function(expr.mat) {
+corTest <- function(expr.mat) {
     r <- cor(expr.mat)
     n <- nrow(expr.mat)
 
@@ -13,56 +13,88 @@ pcorTest <- function(expr.mat) {
     return(list(r=r, p=p))
 }
 
-corPermTest <- function(expr.mat) {
-    #test using permutation tests
-    expr.mat1 <- expr.mat[sample(nrow(expr.mat)), ]
-    return(expr.mat1)
-}
-
 buildNet <- function(cor.res, tfs, p.cutoff) {
     sign <- cor.res$p < p.cutoff
 
     signf <- which(sign == FALSE)
-    signt <- which(sign == TRUE)
-
-    cor.res$r[signt] = 1
     cor.res$r[signf] = 0
     adj.mat <- cor.res$r
 
-    badj.mat <- adj.mat[which(tfs == TRUE), which(tfs==FALSE)]
-    g <- graph.incidence(badj.mat)
+    genes <- which(!(colnames(adj.mat) %in% tfs))
+    badj.mat <- adj.mat[tfs, genes]
+    g <- graph.incidence(badj.mat, weighted="weight")
 
-    return(list(g, badj.mat))
+    return(g)
 }
 
-buildNetCor <- function(expr.data, tfs, p.cutoff) {
+buildNetCor <- function(expr.data, tfs, p.cutoff, perm=FALSE) {
     expr.mat <- t(as.matrix(expr.data))
-    cor.res <- corr.test(expr.mat)
+    cor.res <- corTest(expr.mat)
 
     return(buildNet(cor.res, tfs, p.cutoff))
 }
 
 buildNetPCor <- function(expr.data, tfs, p.cutoff) {
     expr.mat <- t(as.matrix(expr.data))
-    pcor.res <- pcorTest(expr.mat)
+    inf.cor <- cor(expr.mat)
+    inf.pcor <- cor2pcor(inf.cor)
 
-    return(buildNet(pcor.res, tfs, p.cutoff))
+    test.results <- ggm.test.edges(inf.pcor)
+    net.edges <- test.results[test.results$pval < p.cutoff, ]
+
+    nodes1 <- colnames(inf.cor)[net.edges$node1]
+    nodes2 <- colnames(inf.cor)[net.edges$node2]
+
+    net.edges.df <- data.frame(nodes1, nodes2, weight=net.edges$pcor)
+
+    g <- graph.data.frame(net.edges.df)
+
+    adj <- as.matrix(get.adjacency(g, attr="weight"))
+    genes <- which(!(colnames(adj) %in% tfs))
+    badj.mat <- adj[tfs, genes]
+    g.bp <- graph.incidence(badj.mat, weighted="weight")
+
+    return(g.bp)
 }
 
-runNetworkBuilds <- function() {
-    load("data/annotation.RData")
-    expr.data <- read.table(file="data/disc_set/discovery_ExpressionMatrix_red.txt",
-                            header=T, comment.char="", row.names=1)
-    tfs <- read.table("data/disc_set/tfs.txt")
+buildNetMI <- function(g.MI, tfs, annotation) {
+    adj <- as.matrix(get.adjacency(g.MI))
+    pids <- annotation$probeID[which(annotation$probeID %in% rownames(adj))]
+    tfs <- tfs[which(tfs %in% pids)]
+    badj.mat <- adj[tfs, genes]
+    g <- graph.incidence(badj.mat)
 
-    cor.g <- buildNetCor(expr.data, tfs, 0.01)
-    pcor.g <- buildNetPCor(expr.data, tfs, 0.01)
+    return(g)
 }
 
-olapAnalysis <- function(badj.mat) {
+olapAnalysis <- function(badj.mat, vis=FALSE) {
     olaps <- badj.mat %*% t(badj.mat)
+    l.regs <- sort.int(rowSums(olaps), decreasing=T, index.return=T)$ix[1:10]
+    l.olaps <- olaps[l.regs, l.regs]
+    g.olaps <- graph.adjacency(l.olaps, mode="undirected", weighted="weight", diag=FALSE)
 
+    if (vis) {
+        rdp <- RedPort()
+        calld(rdp)
+        addGraph(rdp, g.olaps)
+    }
 
+    return(g.olaps)
 }
+
+
+#write the annotated gene IDs to be used so can do the reduction in python
+load("data/annotation.RData")
+write.table(annotation$probeID, file="data/annotation.dat", quote=FALSE, row.names=FALSE,
+            col.names=FALSE)
+expr.data <- read.table(file="data/disc_set/discovery_ExpressionMatrix_red.txt",
+                            header=T, comment.char="", row.names=1)
+tfs <- annotation[annotation$is.TF == TRUE, 1]
+
+
+
+
+
+
 
 
--- reduce_dataset.py
+++ reduce_dataset.py
@@ -1,29 +1,18 @@
 import sys
 
-def read_annotation(annot_file):
-    ids = dict()
-    for record in open(annot_file):
-        pid, tf = record.split(" ")
-        ids[pid] = tf.strip()
-
-    return ids
-
 def main():
-    ids = read_annotation("annotation.dat")
+    ids = set([line.strip() for line in open("data/annotation.dat")])
 
     out_file = open("data/disc_set/discovery_ExpressionMatrix_red.txt", "wb")
-    tf_file = open("data/disc_set/tfs.txt", "wb")
+
     with open("data/disc_set/discovery_ExpressionMatrix.txt", "r") as f:
         header = f.readline()
-        out_file.write(header.strip())
+        out_file.write(header)
         for record in f:
             rec_id = record.split()[0]
             if rec_id in ids:
                 out_file.write(record)
-                tf_file.write(ids[rec_id]+"\n")
-
 
     out_file.close()
-    tf_file.close()
 
 main()
