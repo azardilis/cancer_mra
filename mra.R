55fc5e2d23d36c71df9abbc682c5c7113c6326e3
Argyris Zardilis <azardilis@gmail.com>
2014-02-27 17:20:54 +0000

    added some function to analyze the hypergeomtric test results from multiple networks

--- mra.R
+++ mra.R
@@ -1,17 +1,35 @@
-library(HTSanalyzeR)
-
-overlapTest  <- function(badj.mat, hits) {
-  univ  <- colnames(badj.mat)
+mraAnalysis <- function(g, exp="Exp1", cont="E2FGF10", p.cutoff=0.05) {
+  adj.mat  <- as.matrix(get.adjacency(g))
+  tfs <- which(get.vertex.attribute(g.mi, "type") == FALSE)
+  badj.mat  <- adj.mat[tfs, ]
+  universe  <- colnames(adj.mat)
+  sigt  <- Fletcher2013pipeline.deg(what=exp)
+  hits  <- get(cont, sigt)
+  hits  <- hits[which(hits %in% universe)]
+  
   gene.sets <- apply(badj.mat, 1, function(x) { names(which(x == 1))})
-  
-  hgt.results  <- multiHyperGeoTest(gene.sets, univ, hits)
-  
-  return(hgt.results)
+  hgt.results  <- as.data.frame(multiHyperGeoTest(gene.sets, universe, hits))
+  sign.tfs  <- hgt.results[which(hgt.results$Pvalue < p.cutoff), ]
+    
+  return(sign.tfs)
 }
 
-sigt  <- Fletcher2013pipeline.deg(what="Exp1")
-hits  <- sigt$E2FGF10
+#correlation network
+res <- vector(mode = "list", length = 3)
+contrasts  <- c("E2FGF10", "E2AP20187", "TetE2FGF10")
+for(i in 1:3) {
+  print(i)
+  hgt.result <- mraAnalysis(g.pcor, exp=paste("Exp", i, sep=""), cont=contrasts[i])
+  res[[i]]  <- hgt.result
+}
 
-hgt.results  <- overlapTest(badj.mat, hits)
-top.tfs  <- names(sort(hgt.results[, 'Observed Hits'], decreasing=T)[1:5])
-#get the names from annotation
+fg.ids  <- lapply(res, rownames)
+tfs.s  <- Reduce(intersect, fg.ids)
+annotation[which(annotation$probeID %in% tfs.s)]
+
+
+
+
+
+
+
