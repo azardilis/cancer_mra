3f20fa81fa44b40521436ba3cecdf6d3288fb1c1
Argyris Zardilis <azardilis@gmail.com>
2014-02-27 00:17:26 +0000

    removed a redundant if statement

--- nd.R
+++ nd.R
@@ -11,21 +11,24 @@ ndReg <- function(cor, beta=0.5, alpha=0.1) {
     xy <- which((tf.net != t(tf.net)) != 0, arr.ind=T)
 
     tf.net.final <- tf.net
-    for (i in 1:nrow(xy)) {
+    if (dim(xy)[1] != 0) {
+      for (i in 1:nrow(xy)) {
         x <- xy[i, ][1]
         y <- xy[i, ][2]
         if (tf.net[x, y] != 0 & tf.net[x, y] != 0) {
-            tf.net.final[x, y] <- (tf.net[x,y] + tf.net[y, x]) / 2
-            tf.net.final[y, x] <- tf.net.final[x, y]
+          tf.net.final[x, y] <- (tf.net[x,y] + tf.net[y, x]) / 2
+          tf.net.final[y, x] <- tf.net.final[x, y]
         } else if (tf.net[x, y] == 0) {
-            tf.net.final[x, y] <- tf.net[y, x]
-            tf.net.final[y, x] <- tf.net.final[x, y]
+          tf.net.final[x, y] <- tf.net[y, x]
+          tf.net.final[y, x] <- tf.net.final[x, y]
         } else if (tf_net[y,x] == 0) {
-            tf.net.final[x, y] -> tf.net[x, y]
-            tf.net.final[y, x] -> tf.net.final[x, y]
+          tf.net.final[x, y] -> tf.net[x, y]
+          tf.net.final[y, x] -> tf.net.final[x, y]
         }
-
+        
+      }  
     }
+    
     cor[1:n.tf, 1:n.tf] <- tf.net.final
 
     y <- quantile(c(cor), 1-alpha)
@@ -38,18 +41,18 @@ ndReg <- function(cor, beta=0.5, alpha=0.1) {
 
     eig.res <- NULL
 
-    if (rcond(eig.res$vectors) < 10^-10) {
-        rp <- 0.001
-        rand.tf <- rp * matrix(runif(n.tf*n.tf), nrow=n.tf)
-        rand.tf <- (rand.tf + t(rand.tf)) / 2
-        diag(rand.tf) <- 0
-        rand.target <- rp * matrix(runif(n.tf*(n-n.tf)), nrow=n.tf)
-        mat.rand <- cbind(rand.tf, rand.target)
-        mat.th <- mat.th + mat.rand
-        mat.th <- rbind(mat.th, matrix(rep(0, (n-n.tf)*n), nrow=(n-n.tf)))
-        eig.res <- eigen(mat.th)
-    }
-
+    #if (rcond(eig.res$vectors) < 10^-10) {
+    rp <- 0.001
+    rand.tf <- rp * matrix(runif(n.tf*n.tf), nrow=n.tf)
+    rand.tf <- (rand.tf + t(rand.tf)) / 2
+    diag(rand.tf) <- 0
+    rand.target <- rp * matrix(runif(n.tf*(n-n.tf)), nrow=n.tf)
+    mat.rand <- cbind(rand.tf, rand.target)
+    mat.th <- mat.th + mat.rand
+    mat.th <- rbind(mat.th, matrix(rep(0, (n-n.tf)*n), nrow=(n-n.tf)))
+    
+    eig.res <- eigen(mat.th)
+    
     U <- eig.res$vectors
     D <- matrix(rep(0, nrow(mat.th)*nrow(mat.th)), nrow=nrow(mat.th))
     diag(D) <- eig.res$values
@@ -72,4 +75,23 @@ ndReg <- function(cor, beta=0.5, alpha=0.1) {
 
     return(mat.nd)
 }
+
+edges  <- read.table("dream/net1_expression_data_Correlation_predictions.txt")
+colnames(edges)[3]  <- "weight"
+g.df  <- graph.data.frame(edges)
+adj.mat  <- abs(as.matrix(get.adjacency(g.df, attr="weight")))
+tfs  <- as.character(read.table("dream/net1_transcription_factors.tsv")$V1)
+
+badj.mat  <- adj.mat[tfs, ]
+
+edges.out  <- read.table("dream/net1_expression_data_Pearson-ND_predictions.txt")
+colnames(edges.out)[3]  <- "weight"
+g.out  <- graph.data.frame(edges.out)
+adj.out  <- abs(as.matrix(get.adjacency(g.out, attr="weight")))
+
+badj.out  <- adj.out[tfs, ]
+
+
+
+
 
